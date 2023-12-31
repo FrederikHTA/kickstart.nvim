ea1101025cf2e486bdcb599fa0a9585082bfba66
Frederik Harpøth Træholt <ftr@mft-energy.com>
2023-12-31 23:00:07 +0100

    stuff

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -12,21 +12,6 @@ return {
   config = function()
     require('neo-tree').setup {
       enable_git_status = true,
-      git_status = {
-        symbols = {
-          -- Change type
-          added     = "✚", -- or "✚", but this is redundant info if you use git_status_colors on the name
-          modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
-          deleted   = "✖", -- this can only be used in the git_status source
-          renamed   = "󰁕", -- this can only be used in the git_status source
-          -- Status type
-          untracked = "",
-          ignored   = "",
-          unstaged  = "󰄱",
-          staged    = "",
-          conflict  = "",
-        }
-      }
     }
   end,
 }

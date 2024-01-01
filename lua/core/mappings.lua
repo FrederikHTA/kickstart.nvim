4f79ce58ec6938c216589da98e68850a00d9ce08
Frederik Harpøth Træholt <ftr@mft-energy.com>
2024-01-01 17:39:32 +0100

    change mappings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -18,6 +18,7 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+-- navigation keymaps
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 vim.keymap.set('n', '<C-u>', '<C-u>zzzv')
@@ -25,14 +26,28 @@ vim.keymap.set('n', '<C-d>', '<C-d>zzzv')
 vim.keymap.set('n', '<C-f>', '<C-f>zzzv')
 vim.keymap.set('n', '<C-b>', '<C-b>zzzv')
 
+-- visual mode keymaps
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
 -- Git
-vim.keymap.set('n', '<leader>gf', '<cmd>G fetch')
+vim.keymap.set('n', '<leader>gf', '<cmd>G fetch<cr>')
+vim.keymap.set('n', '<leader>gp', '<cmd>G push<cr>')
+vim.keymap.set('n', '<leader>gu', '<cmd>G pull<cr>')
 
 -- paste and keep in black hole register
 vim.keymap.set('v', '<leader>p', '"_dP')
+vim.keymap.set('n', '<leader>d', '\"_d')
+vim.keymap.set('v', '<leader>d', '\"_d')
 
 -- nvim-tree
 vim.keymap.set('n', '<leader>n', '<cmd>Neotree filesystem toggle left<cr>', opts)
 
 -- save
 vim.keymap.set('n', '<leader>w', '<cmd>silent write<cr>', opts)
+vim.keymap.set('n', '<leader>q', '<cmd>q<cr>', opts)
+
+-- code actions
+vim.keymap.set('n', '<leader>cr', function()
+	vim.lsp.buf.format()
+end)
